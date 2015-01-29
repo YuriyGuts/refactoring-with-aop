079da73c072370be65f8465b31ad832a804626b9
Yuriy Guts <yuriy.guts@gmail.com>
2015-01-29 11:22:06 +0200

    Cross-cutting mess, part 1: logging on method boundaries.

--- src/StackOverflowLite/StackOverflowLite.Service.Implementation/ReputationService.cs
+++ src/StackOverflowLite/StackOverflowLite.Service.Implementation/ReputationService.cs
@@ -1,4 +1,5 @@
-﻿using StackOverflowLite.Data.Contracts;
+﻿using System;
+using StackOverflowLite.Data.Contracts;
 using StackOverflowLite.Domain;
 using StackOverflowLite.Service.Contracts;
 
@@ -15,15 +16,23 @@ namespace StackOverflowLite.Service.Implementation
 
         public int AddReputationForQuestion(Question question)
         {
+            Console.WriteLine("{0:HH:mm:ss.fff}: AddReputationForQuestion started.", DateTime.Now);
+
             int pointsToAdd = question.UpVotes * 5 - question.DownVotes * 2;
             int newReputation = _userDataService.AddReputationForUser(question.Author, pointsToAdd);
+
+            Console.WriteLine("{0:HH:mm:ss.fff}: AddReputationForQuestion completed.", DateTime.Now);
             return newReputation;
         }
 
         public int AddReputationForAnswer(Answer answer)
         {
+            Console.WriteLine("{0:HH:mm:ss.fff}: AddReputationForAnswer started.", DateTime.Now);
+
             int pointsToAdd = answer.UpVotes * 10 - answer.DownVotes * 3;
             int newReputation = _userDataService.AddReputationForUser(answer.Author, pointsToAdd);
+
+            Console.WriteLine("{0:HH:mm:ss.fff}: AddReputationForAnswer completed.", DateTime.Now);
             return newReputation;
         }
     }
